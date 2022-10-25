63cdb102f9a644739c03b61c55bf0bc5248f1160
Robert Connolly <robertoconnolly100@gmail.com>
2022-10-25 23:29:15 +0200

    sumatorio_lista

--- suma_lista.py
+++ suma_lista.py
@@ -1,23 +1,24 @@
-resultado = []
+
 def sumatorio_lista(lista: list):
     n = 0
+    r = 0
     items_in_list = len(lista)
-    return sumatorio_lista2(lista, n, items_in_list)
+    return sumatorio_lista2(lista, n, items_in_list, r)
 
 
-def sumatorio_lista2(lista, n:int, items_in_list):
+def sumatorio_lista2(lista, n:int, items_in_list, r):
         if n == items_in_list:
-            return #basecase
+            return r #basecase
         
-        if type(lista[n]) == "list":
-            sumatorio = sum(lista[n])
-            resultado.append(sumatorio)
-            return sumatorio_lista2(lista, n+1, items_in_list)
+        if isinstance(lista[n], list):
+            for i in range(len(lista[n])):
+                r = r + i
+            return sumatorio_lista2(lista, n+1, items_in_list, r)
 
         else:
-            resultado.append(lista[n])
-            return sumatorio_lista2(lista, n+1, items_in_list)
+            r = lista[n] + r
+            return sumatorio_lista2(lista, n+1, items_in_list, r)
 
 
 lista_numeros = [2, 3, 4, [2, 3]]
-print(sumatorio_lista(lista_numeros), resultado)
+print(sumatorio_lista(lista_numeros))
