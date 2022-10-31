afdf177df858da40eb3fe2551b204cabe4089097
Robert Connolly <robertoconnolly100@gmail.com>
2022-10-31 12:05:04 +0100

    Torredehanoi

---
+++ TorreDeHanoi.py
@@ -0,0 +1,16 @@
+from ast import Str
+
+
+def TorreDeHanoi(n, principio, final, intermedio):
+    if n == 1:
+        print("Mueve primer disco de", principio, " a ", final)
+    else:
+        TorreDeHanoi(n-1, principio, intermedio, final)
+        print("Moviendo disco: " + str(n) + " desde: "+ principio + " a " + final)
+        TorreDeHanoi(n-1, intermedio, final, principio)
+
+
+
+
+TorreDeHanoi(2, "A", "C", "B")
+
